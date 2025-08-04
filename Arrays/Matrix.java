f5b2895e020053af1061620ef5c1be08d4440f41
Rohith.M <rohithmerugu2423@gmail.com>
2025-08-04 09:04:53 +0530

    Add files via upload

---
+++ Arrays/Matrix.java
@@ -0,0 +1,40 @@
+package Arrays;
+import java.util.*;
+public class Matrix 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] a= new int[n][n];
+		int[][] b= new int[n][n];
+		int i,j,c;
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				a[i][j] = sc.nextInt();
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				b[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				c=a[i][j]+b[i][j];
+				System.out.print(c + " ");
+			}
+			System.out.println("");
+			
+		}
+		
+		sc.close();
+	}
+}
---
+++ Arrays/Transpose.java
@@ -0,0 +1,33 @@
+package Arrays;
+import java.util.*;
+public class Transpose 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] a= new int[n][n];
+		
+		int i,j;
+		
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				a[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				System.out.print(a[j][i]+" ");
+			}
+			System.out.println("");
+		}
+		
+		
+		sc.close();
+	}
+}
